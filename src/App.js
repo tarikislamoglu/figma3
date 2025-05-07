a904b55f68b65f59beafa4967a735afb9ef62724
Tarık İslamoğlu <tarikislamoglu8@gmail.com>
2025-05-07 20:28:15 +0300

    figma3fnl

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" h-screen w-full">
+      <div className="flex items-center border-1  ">
+        <div className="bg-[#1AEBB7] h-screen w-2/5 flex justify-center items-center ">
+          <img src="/lion.png" className="max-w[400px] max-h-[400px] z-1" />
+        </div>
+        <div className=" bg-white flex flex-col w-3/5  h-screen justify-center">
+          <form className="flex flex-col space-y-4 p-10">
+            <h2 className="text-center text-3xl font-semibold text-[#000000B2]">
+              LOGIN
+            </h2>
+            <input
+              placeholder="Username"
+              className="p-1 border-1 border-[#0000004F] text-sm"
+            />
+            <input
+              placeholder="Password"
+              className="p-1 border-1 border-[#0000004F] text-sm"
+            />
+            <div className="flex flex-col md:flex-row  md:justify-between w-full  space-y-4 md:space-y-0">
+              <button className="bg-[#1AEBB7] text-white p-2 w-full md:w-1/2">
+                LOGIN
+              </button>
+              <div className="flex md:flex-col justify-between w-full md:items-end">
+                <a href="" className="text-sm">
+                  Forgot Password
+                </a>
+                <a href="" className="text-sm">
+                  Register
+                </a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
